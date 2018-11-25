c7b9861c5ed3724bb39214cce55cf267c619935e
santiagoziel <45303560+santiagoziel@users.noreply.github.com>
2018-11-24 16:38:03 -0800

    Update sensoresjava.js

--- sensoresjava.js
+++ sensoresjava.js
@@ -9,9 +9,16 @@
     messagingSenderId: "726116796060"
   };
   firebase.initializeApp(config);
+
+  // create references
+  var refmed = firebase.database().ref().child('sensores');
   var refpreguntas = firebase.database().ref().child('preguntas');
+
+  //get stuff from html
+	var table = document.getElementById("table")
   var boton = document.getElementById("enviar");
 
+	// send data on click
   boton.onclick = function(){
     var tiempo_ = (document.getElementById('a').value * 3600) + (document.getElementById('b').value * 60) + (document.getElementById('c').value);
     var med_ = document.getElementById('med').value;
@@ -23,4 +30,37 @@
     };
     refpreguntas.set(datos);
   };
+
+  refmed.on('value', snap => {
+      // do stuff when database updates
+    	sense = snap.val().parametroquesemide;
+    	seconds = snap.val().segundos;
+
+    	var row = table.insertRow(0);
+      var cell_1 = row.insertCell(0);
+      var cell_2 = row.insertCell(1);
+      var cell_3 = row.insertCell(2);
+
+      switch(sense) {
+        case 0:
+          value = snap.val().temperatura;
+          variable_midiendo = "temperatura"
+          break;
+        case 1:
+        	value = snap.val().humedad;
+          variable_midiendo = "humedad"
+          break;
+        case 2:
+          variable_midiendo = "distancia"
+          value = snap.val().distancia;
+      }
+
+
+    	cell_1.innerHTML = variable_midiendo;
+    	cell_2.innerHTML = seconds;
+      cell_3.innerHTML = value;
+	});
+
+  console.log (refmed.value)
+
 }());
